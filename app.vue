ae007c09a2989c1149059b2e20760001659eda43
Ali Karam <AK@alikaram.com>
2022-07-24 21:20:51 +0300

    feat: add daisyUI

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
-    <div class="flex justify-center items-center h-screen">
+    <div class="flex justify-center items-center m-10">
       <h1 class="text-3xl font-bold text-green-500">
-        How to install Tailwind CSS 3 in Nuxt 3
+        Tailwind CSS 3 in Nuxt 3
       </h1>
+    </div>
+    <div class="flex justify-center items-center m-10">
+      <button class="btn btn-primary">Button</button>
     </div>
   </div>
 </template>
