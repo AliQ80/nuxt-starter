ad4739e12c130c8e3f49481e3a2f965965fed1b9
Ali Karam <AK@alikaram.com>
2022-09-10 22:09:28 +0300

    feat: :sparkles: add profile page

---
+++ pages/ProfilePage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: 'Nuxt-starter | profile Page',
+    alias: ['/profile'],
+    middleware: ['auth'],
+    // or middleware: 'auth'
+  })
+</script>
+
+<template>
+  <div>
+    <h1>profile page</h1>
+  </div>
+</template>
+
+<style scoped></style>
