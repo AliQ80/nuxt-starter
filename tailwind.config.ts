09a1db10e38955fd2d06c3ddcb0d25499169349d
Ali Karam <AK@alikaram.com>
2022-10-05 23:21:00 +0300

    feat(WIP): :sparkles: add google fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,11 @@ module.exports = {
     'app.{js,ts,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lobster: "'Lobster', cursive",
+      },
+    },
   },
   plugins: [require('daisyui'), require('@tailwindcss/typography')],
 }
